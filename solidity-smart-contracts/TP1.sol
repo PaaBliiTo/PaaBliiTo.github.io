7a8cb2063b4707dddbc9e90e61382e238d997250
Fabio <fabiobenoit@hotmail.fr>
2017-03-22 16:20:14 +0100

    Last commit TP Blockchain

--- solidity-smart-contracts/TP1.sol
+++ solidity-smart-contracts/TP1.sol
@@ -15,6 +15,8 @@ contract TP1 {
 	}
 
 	address chairperson;
+
+	//Mapping of voters
 	mapping(address => Voter) voters;
 	Proposal[] proposals;
 
@@ -23,16 +25,24 @@ contract TP1 {
 		proposals.length = _numProposals;
 	}
 
+	//Let a user vote
 	function vote(uint8 proposal) {
+
+		//Get the voter calling the contract
 		Voter sender = voters[msg.sender];
+
+		//Check if the user already voted or if the number of the proposal is incorrect
 		if(sender.voted || proposal >= proposals.length) return;
 		sender.voted = true;
 		sender.vote = proposal;
 		proposals[proposal].voteCount += 1;
 	}
 
+	//Return the proposal that won the vote
 	function winningProposal() constant returns (uint8 winningProposal) {
 		uint256 winningVoteCount = 0;
+
+		//Checks for every proposal what is the one which has the most votes
 		for(uint8 proposal = 0; proposal < proposals.length; proposal++){
 			if(proposals[proposal].voteCount > winningVoteCount) {
 				winningVoteCount = proposals[proposal].voteCount;
--- solidity-smart-contracts/TP2.sol
+++ solidity-smart-contracts/TP2.sol
@@ -1,13 +1,46 @@
 pragma solidity ^0.4.0;
 
 
-//Managing a fidelity points and local currency thanks to Solidity Smart Contract
-
+//Fidelity points and local currency
 
 contract TP2 {
 
-	struct  {
-	}
-
-	
+    //User who has fidelity points
+    struct User{
+        uint fidelityPoints;
+    }
+    
+    //Mapping of users allowed to have fidelity points
+    mapping(address => User) users;
+    
+    //get the number of fidelityPoints a user has
+    function balanceOf(address _owner) constant returns (uint256 balance){
+        balance = users[_owner].fidelityPoints;
+    }
+    
+    //Give the user a certain number of fidelityPoints
+    function transfer(address _to, uint256 _value) returns (bool success){
+        
+        //Verify if the value is not negative
+        if(_value > 0){
+            users[_to].fidelityPoints += _value;
+            success = true;
+        }
+        else{
+            success = false;
+        }
+    }
+    
+    //Remove a certain number of fidelity points from a user
+    function transferFrom(address _from, address _to, uint256 _value) returns (bool success){
+        
+        //Verify if the value is not negative
+        if(_value > 0){
+            users[_from].fidelityPoints -= _value;
+            success = true;
+        }
+        else{
+            success = false;
+        }
+    }
 }
